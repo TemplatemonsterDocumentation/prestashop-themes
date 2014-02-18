2d71db35b4ddb77dc4713c2ebe408ac53a8de22d
Slavito <vyacheslav.lyovkin@gmail.com>
2014-02-18 10:16:11 +0200

    Current Year fixed

--- js/script.js
+++ js/script.js
@@ -24,7 +24,11 @@ $(document).ready(function() {
 		if ($(location).attr('href').indexOf("http://info.template-help.com")+1)
 		{
 			$('#copyright').text('Template-Help.com');
-		}			  
+		}	
+
+		// current year
+			  var date = new Date();
+			  $("#date").html(date.getFullYear());
 
  		// slide-down-box-------------------------------  
       var idArray = [],
