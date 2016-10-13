8602bc3f039068aae4142596c9455c5464b19f1b
Cf-251 <hpevneva@gmail.com>
2016-10-13 18:03:43 +0300

    EDIT: description file

--- sections/about-eveprest/en/__description.php
+++ sections/about-eveprest/en/__description.php
@@ -1,12 +1,1 @@
 <h2>About Eveprest</h2>
-
-<div class="small">
-    <p>Available in:</p>
-    <ul class="marked-list-type-2">
-        <li>Fashion</li>
-        <li>Electronics</li>
-        <li>Spare Parts</li>
-        <li>Furniture</li>
-        <li>One Product</li>
-    </ul>
-</div>
