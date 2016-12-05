2aa13cc75ecfba20eaa2b6f462dd263bda66c135
Cf-251 <hpevneva@gmail.com>
2016-12-05 11:37:08 +0200

    EDIT: images, Tutorials section; ADD: default project title caption

--- sections/basic-tutorials/en/edit_slider.php
+++ sections/basic-tutorials/en/edit_slider.php
@@ -1,29 +1,28 @@
 <h3>How to edit slider</h3>
 
-<p>First find the <strong>Image slider for your homepage</strong> module in <strong>Modules list</strong> of <strong>Modules and services</strong> menu tab of your PrestaShop store (1) (it is located
-    in the <strong>Front Office Features</strong> modules section). Then press the 'Configure' button (2) to edit the module.
+<p>First navigate to Modules -> Modules and Services in the admin panel of your PrestaShop store (1) and open the tab 'Installed modules'. Then find the <strong>Image slider</strong> module and press the 'Configure' button (2) to edit the module.
 </p>
 
 <figure class="img-polaroid">
-    <img src="img/12-slider-1.png" alt=""/>
+    <img src="img/image-slider-1.png" alt=""/>
 </figure>
 
-<p>On the screen that appears next you'll be able to set the main properties of your slider like maximum width of images, speed of changing slides, the delay between two slides and autoplay option. <br>
+<p>On the screen that appears next you'll be able to set the main properties of your slider like speed of changing slides, the pause on hover and loop option.<br>
 After you've changed it, press the 'Save' button (1).</p>
 
 <figure class="img-polaroid">
-    <img src="img/12-slider-2.png" alt=""/>
+    <img src="img/image-slider-2.png" alt=""/>
 </figure>
 
 <p>To add a new slide find the '+' button in the top right corner of Slides list section (1) and click it. <br>
 If you want to edit the existing slide, click the 'Edit' button (2).</p>
 
 <figure class="img-polaroid">
-    <img src="img/12-slider-3.png" alt=""/>
+    <img src="img/image-slider-3.png" alt=""/>
 </figure>
 
 <p>Further process is quite simple. In 'Slide Information' section fill all the required fields and 'Save' (1) it.</p>
 
 <figure class="img-polaroid">
-    <img src="img/12-slider-4.png" alt=""/>
+    <img src="img/image-slider-4.png" alt=""/>
 </figure>
