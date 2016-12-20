c61d45a9a268d2dda79b5054d19334dc3dc4e91e
iceberg <iceberg@templatemonster.me>
2016-12-20 19:29:54 +0200

    ADD: TM Products Manager module documentation

---
+++ sections/custom-modules/en/tmproductsmanager.php
@@ -0,0 +1,106 @@
+<h3>TM Products Manager</h3>
+<h4>Introduction</h4>
+<p>The module is created for managing online shops, especially shops with big amount of products. Using this module you can easily filter your products and make different operations on them.</p>
+
+<h4>Installation and removal of the module</h4>
+<p>The module is installed and removed as any Prestashop module.</p>
+<p class="alert alert-info">
+    <span>Removal or re-installation of the module will not reset the changes you made using it.</span>
+</p>
+<p>When the module has been successfully installed, go to Catalog -> TM Products Manager (1) to see the module's main menu.</p>
+<figure class="img-polaroid"><img src="img/tmproductsmanager-1.png" alt=""/></figure>
+<h4>Performance</h4>
+<p>The main module menu is divided into two main parts: 'Choose section' (1) and 'Choose option' (2).</p>
+<figure class="img-polaroid"><img src="img/tmproductsmanager-2.png" alt=""/></figure>
+<p>The first part is created in the form of tabs - Information, Price, Associations and Quantity.</p>
+<figure class="img-polaroid"><img src="img/tmproductsmanager-3.png" alt=""/></figure>
+<ul class="marked-list">
+    <li><strong>Information</strong> - options concerning product availability and description.</li>
+    <li><strong>Price</strong> - price and tax rules settings.</li>
+    <li><strong>Associations</strong> - ability to change brand, category or accessories of several products.</li>
+    <li><strong>Quantity</strong> - changing products' quantity.</li>
+</ul>
+<ul class="index-list">
+    <li>
+        <p>Click each tab to see the available options in this section. They will be described further. <br>
+            Each option has a hint - short description, which appears in the right when you hover the option (1).</p>
+        <figure class="img-polaroid"><img src="img/tmproductsmanager-4.png" alt=""/></figure>
+    </li>
+    <li>
+        <p>Choose the needed option and click it.</p>
+    </li>
+    <li>
+        <p>The next step is filtering products. Set the filtering options according to your needs and click the 'Select' button (1). To clear the selected filters click the 'Reset' button (2).</p>
+        <figure class="img-polaroid"><img src="img/tmproductsmanager-5.png" alt=""/></figure>
+        <p>You can also cancel each of the filters after you've selected them. Simply click the cross (1) near the product property value and it will be removed.</p>
+        <figure class="img-polaroid"><img src="img/tmproductsmanager-6.png" alt=""/></figure>
+    </li>
+    <li>
+        <p>Below the filters there's a list of products matching the selected criteria. You can edit it lf you need to delete some of the products from the list. Each product can be removed separately by clicking 'Remove from the list' button (1). To remove all products from the list, click the 'Select all' (2) and 'Remove selected' (3) buttons. If you checked a large number of products and need to uncheck them, click the 'Unselect all' button (4).</p>
+        <figure class="img-polaroid"><img src="img/tmproductsmanager-7.png" alt=""/></figure>
+    </li>
+    <li>
+        <p>When the list of selected products complies with all requirements, click the 'Process' button (1).</p>
+        <figure class="img-polaroid"><img src="img/tmproductsmanager-8.png" alt=""/></figure>
+    </li>
+    <li>
+        <p>Set the option/options according to your needs and click the 'Save' button (1). If you need to make some changes in products list, click 'Back to filter' button (2).</p>
+        <p class="alert alert-info">
+            <span>Please note, that you won't be able to undo these changes the same way.</span>
+        </p>
+        <figure class="img-polaroid"><img src="img/tmproductsmanager-9.png" alt=""/></figure>
+    </li>
+</ul>
+
+<h4>Available options</h4>
+<h5>Information</h5>
+<p>The section allows to configure the following options:</p>
+<figure class="img-polaroid"><img src="img/tmproductsmanager-10.png" alt=""/></figure>
+<ul class="marked-list">
+    <li><strong>Enable/Disable products</strong> - set the on/off status of your products.</li>
+    <li><strong>Available for order</strong> - make some products available or unavailable for order.
+        <p class="alert alert-warning small">
+            <span>NOTE: if 'Available for order' is set to 'Yes' for the products with hidden prices, their prices will automatically become visible again.</span>
+        </p>
+    </li>
+    <li><strong>Show price</strong> - show or hide product price.
+        <p class="alert alert-warning small">
+            <span>NOTE: if you set 'Show price' to 'No' for the products, which are available for order, they will be automatically changed to unavailable.</span>
+        </p>
+    </li>
+    <li><strong>Online only</strong> - apply the 'Online only' mark to the selected items.</li>
+    <li><strong>Condition</strong> - change the condition of multiple products.</li>
+    <li><strong>Short description</strong> - add a short description which should be applied to all selected products.</li>
+    <li><strong>Full description</strong> - add a description to selected products with an ability to place it before the main description, after it, or replace the whole description with the new one.</li>
+    <li><strong>Tags</strong> - add more tags to the chosen products.</li>
+</ul>
+
+<h5>Price</h5>
+<p>In this section you can change options concerning product price.</p>
+<figure class="img-polaroid"><img src="img/tmproductsmanager-11.png" alt=""/></figure>
+<ul class="marked-list">
+    <li><strong>Display "On sale"</strong> - display the selected items with 'On sale' mark.</li>
+    <li><strong>Change tax rules</strong> - choose the tax rules which will be applied to several products.</li>
+    <li><strong>Add new specific price</strong> - set the special price for selected products with an ability to add the price activation period.</li>
+</ul>
+
+<h5>Associations</h5>
+<p>Here you can change options connected with product relations.</p>
+<figure class="img-polaroid"><img src="img/tmproductsmanager-12.png" alt=""/></figure>
+<ul class="marked-list">
+    <li><strong>Change brand</strong> - change brand of selected items.</li>
+    <li><strong>Change default category</strong> - choose the category which will be set as default for some products.
+        <p class="alert alert-warning small">
+            <span>NOTE: You can choose only one default category. Keep in mind that your current default category will be replaced with the new one.</span>
+        </p>
+    </li>
+    <li><strong>Change related categories</strong> - set the related categories to multiple products with an ability to save or remove old relations.</li>
+    <li><strong>Add accessories</strong> - add the accessories suitable for the chosen products.</li>
+</ul>
+
+<h5>Quantity</h5>
+<p>This option allows to set the product quantity or increase/decrease current quantity by a certain amount.</p>
+<figure class="img-polaroid"><img src="img/tmproductsmanager-13.png" alt=""/></figure>
+<ul class="marked-list">
+    <li><strong>Change quantity</strong> - set the required quantity of some products.</li>
+</ul>
