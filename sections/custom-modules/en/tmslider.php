90dab09fc4c5c568da1da5ec29d696f9c2f143ce
Cf-251 <hpevneva@gmail.com>
2017-03-10 11:31:05 +0200

    ADD: Sweet4you documentation

---
+++ sections/custom-modules/en/tmslider.php
@@ -0,0 +1,5 @@
+<h3>TM Slider</h3>
+<p>The module is specially crafted to demonstrate your content in an efficient way. It features all you need to showcase your content and make a professionally looking slider. The module has plenty of customization tools, which make the slider super-easy to adjust.</p>
+
+<h4>Module Installation</h4>
+<p>This module can be installed/uninstalled as any other PrestaShop module.</p>
