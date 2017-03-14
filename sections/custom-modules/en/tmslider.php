3fadcaf3b94acc3c5c6ae020026dd621b30c6a88
Cf-251 <hpevneva@gmail.com>
2017-03-14 11:27:35 +0200

    ADD: BigBike documentation

--- sections/custom-modules/en/tmslider.php
+++ sections/custom-modules/en/tmslider.php
@@ -1,40 +1,5 @@
 <h3>TM Slider</h3>
-<p>The module is specially crafted to demonstrate your content in an efficient way. It features all you need to showcase your content and make a professionally looking slider. The module has plenty of customization tools, which make the slider super-easy to adjust. </p>
+<p>The module is specially crafted to demonstrate your content in an efficient way. It features all you need to showcase your content and make a professionally looking slider. The module has plenty of customization tools, which make the slider super-easy to adjust.</p>
 
 <h4>Module Installation</h4>
 <p>This module can be installed/uninstalled as any other PrestaShop module.</p>
-
-
-<h4>Module settings</h4>
-<p>After module has been installed you can configure it on the module configuration
-    page.</p>
-<p>In order to get required data for module configuration you need to create
-    corresponding application on the needed resource.</p>
-<p>It is not necessary to use both variants of sign in (Google, Facebook)
-    simultaneously, you can use one of them only.</p>
-<h4>Facebook Fields</h4>
-<ul class="marked-list">
-    <li>ID of your App</li>
-    <li>Secret key of your App</li>
-</ul>
-<h4>Google Fields</h4>
-<ul class="marked-list">
-    <li>ID of your App</li>
-    <li>Secret key of your App</li>
-    <li>Redirect path after sign in <strong>(your shop
-            URL+index.php?fc=module&module=tmsociallogin&controller=googlelogin)</strong></li>
-</ul>
-<h4>VK Fields</h4>
-<ul class="marked-list">
-    <li>ID of your App</li>
-    <li>Secret key of your App</li>
-    <li>Redirect path after sign in <strong>(your shop
-            URL+index.php?fc=module&module=tmsociallogin&controller=vklogin)</strong></li>
-</ul>
-<p class="alert alert-info">More detailed information you can get by following the links: <a href="https://developers.facebook.com/docs/apps/register" target="_blank">Facebook App</a>, <a href="https://support.google.com/cloud/answer/6158849?hl=en&ref_topic=6262490" target="_blank">Google App</a> and <a href="https://vk.com/dev/web_how_to_start">VK App</a>.</p>
-<p class="alert alert-warning">
-    Redirect path must be the same that has been indicated while creating Google App or VK App.
-</p>
-<figure class="img-polaroid">
-    <img src="img/tmsociallogin120-1.png" alt=""/>
-</figure>
