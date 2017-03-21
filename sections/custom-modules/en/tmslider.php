59153903c4e6746113c6d7b04e97680108c9af2b
Cf-251 <hpevneva@gmail.com>
2017-03-21 10:59:58 +0200

    ADD: Eveprest Furniture documentation

--- sections/custom-modules/en/tmslider.php
+++ sections/custom-modules/en/tmslider.php
@@ -3,3 +3,90 @@
 
 <h4>Module Installation</h4>
 <p>This module can be installed/uninstalled as any other PrestaShop module.</p>
+
+<h4>Module customization and managing</h4>
+<h5>To implement a slider into your site, follow the next steps:</h5>
+<ul class="index-list">
+    <li>Add new slides</li>
+    <li>Create slider from slides</li>
+    <li>Manage relations</li>
+</ul>
+
+<h5>Adding new slide</h5>
+<ul class="index-list">
+    <li>
+        <p>Navigate to TM Slider -> TM Slides in Admin panel (1).</p>
+        <figure class="img-polaroid"><img src="" alt="" /></figure>
+    </li>
+    <li>
+        <p>Click the 'Add new' ('+') button (1).</p>
+        <figure class="img-polaroid"><img src="" alt="" /></figure>
+        <ul class="marked-list">
+            <li><strong>Slide name</strong> - enter the slide name</li>
+            <li><strong>Link</strong> - enter the slide link</li>
+            <li><strong>Link target</strong> - choose if the link should be opened in new window or in the same</li>
+            <li><strong>Width</strong> - enter the slide width in px</li>
+            <li><strong>Height</strong> - enter the slide height in px</li>
+            <li><strong>Slide type</strong> - choose the type of slide background (image / youtube video / no slide content)</li>
+            <li><strong>Select an image (default)</strong> - this image will be displayed on the large screens and on other screens if another image is not defined</li>
+            <li><strong>Select an image (tablet)</strong> - this image will be displayed on tablet screens</li>
+            <li><strong>Select an image (mobile)</strong> - this image will be displayed on mobile screens</li>
+            <li><strong>Select an image for Retina screens</strong> - this image will be displayed on retina screens</li>
+            <li><strong>Slide description</strong> - enter the slide description</li>
+            <li><strong>Select an thumbnail image</strong> - choose the image for slide thumbnail</li>
+            <li><strong>Thumbnail text</strong> - enter a thumbnail text</li>
+        </ul>
+    </li>
+    <li>Click 'Save' button (1).</li>
+</ul>
+
+<h5>Adding slide content</h5>
+<ul class="index-list">
+    <li>
+        <p>Navigate to TM Slider -> TM Slides in Admin panel and click the 'View' button (1) or simply click on the slide you want to edit.</p>
+        <figure class="img-polaroid"><img src="" alt="" /></figure>
+        <p>To add a new content layer click the 'Add new' ('+') button (1).</p>
+        <figure class="img-polaroid"><img src="" alt="" /></figure>
+        <ul class="marked-list">
+            <li><strong>Item title</strong> - the title of he content layer</li>
+            <li><strong>Specific class</strong> - specific class applied to the layer</li>
+            <li><strong>Layer content</strong> - content og the layer</li>
+            <li><strong>Active</strong> - option to show or hide the content</li>
+            <li><strong>Item order</strong> - the order of layer (item with highest number will be on top)</li>
+            <li><strong>Position type</strong> - predefined position (choose one of 9 basic positions) or precise position (enter the coordinates of the item in px or %)</li>
+            <li><strong>Show effect</strong> - effect while item is appearing</li>
+            <li><strong>Hide effect</strong> - effect while the item is hiding</li>
+            <li><strong>Show delay</strong> - delay before the item appears</li>
+            <li><strong>Hide delay</strong> - delay before the item disappears</li>
+        </ul>
+    </li>
+    <li>Click 'Save' button (1).</li>
+</ul>
+
+<h5>Creating slider</h5>
+<ul class="index-list">
+    <li>
+        <p>Navigate to TM Slider -> TM Sliders in Admin panel (1).</p>
+    </li>
+    <li>
+        <ul class="marked-list">
+            <li><strong>Select page</strong> - the page where the slider should be displayed</li>
+            <li><strong>Select hook</strong> - the hook for the slider</li>
+            <li><strong>Select slider</strong> - choose the slider</li>
+            <li><strong>Slider order</strong> - the order of sliders (if there are several in the same hook of the same page)</li>
+            <li><strong>Active</strong> - </li>
+            <li><strong>Slide only</strong> - display only the slide without related layouts</li>
+            <li><strong>Slider width(px)</strong> - display only the slide without related layouts</li>
+            <li><strong>Slider height</strong> - display only the slide without related layouts</li>
+            <li><strong>Responsive</strong> - Makes the slider responsive. The slider can be responsive even if the "width" and/or "height" properties are set to fixed values. In this situation, "width" and "height" will act as the maximum width and height of the slides.</li>
+            <li><strong>Allow autopaly</strong> - Indicates whether or not autoplay will be enabled. </li>
+            <li><strong>Autoplay delay</strong> - Sets the delay/interval (in milliseconds) at which the autoplay will run.</li>
+            <li><strong>Autoplay direction</strong> - Indicates whether autoplay will navigate to the next slide or previous slide.</li>
+            <li><strong>Autoplay on hover</strong> - Indicates if the autoplay will be paused or stopped when the slider is hovered. </li>
+            <li><strong>Fade</strong> - Indicates if fade will be used.</li>
+            <li><strong>Fade out previous slide</strong> - Indicates if the previous slide will be faded out (in addition to the next slide being faded in).</li>
+        </ul>
+    </li>
+    <li></li>
+
+</ul>
