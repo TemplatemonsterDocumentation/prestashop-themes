f3901258984ebc7d901840778c83709386738595
Cf-251 <hpevneva@gmail.com>
2017-03-22 16:32:54 +0200

    ADD: Eveprest update documentation FIX: config.php

--- sections/update/en/theme_update.php
+++ sections/update/en/theme_update.php
@@ -2,5 +2,7 @@
 <p class="alert alert-warning">The changes you make may affect your existing store, so you are strongly suggested to back up your site before updating it.</p>
 <p>To get the updated version of Eveprest, you'll need to restore the download link. Next, download the Eveprest archive again.</p>
 <p>To use new theme, you need to add it using the instructions in Template Installation section.</p>
+<p class="alert alert-info">Theme Installator is included into the theme pack. But if you want your site to look exactly like on demo, there are two ways. If you need to install sample data, navigate to 'eveprest-theme -> theme -> sample_data' and open the file 'info.txt'. Then click the link in this file and download sample data. Advanced users can go through the process of manual installation by opening 'info.txt' file in 'eveprest-theme -> theme -> manual_install' and download files the same way.</p>
 <p>To add and use new modules, open the update folder in flagman archive, copy the corresponding folders to your /modules folder and install them from the admin panel.</p>
+
 
