c249d276867d22e0e341ddc3bc1f8574ac27764d
Nikki0 <victoria.panska@gmail.com>
2017-08-16 10:19:52 +0300

    EDIT: TM Google Map module documentation 1.7

--- sections/custom-modules/en/tmgooglemap115.php
+++ sections/custom-modules/en/tmgooglemap115.php
@@ -1,11 +1,11 @@
-<h3>TM Google Map 1.2.0</h3>
+<h3>TM Google Map</h3>
 <p>This module allows adding Google Map with store locations and store short descriptions to the Homepage. The map will display the addresses of all the stores connected with your online store.</p>
 <h5>Installation and removal</h5>
 <p>The module is installed and removed as any other PrestaShop module.</p>
 <h5>Module settings</h5>
 <p>After installing the module, you can add stores to the map. After pressing the 'Add new store' button (1), you'll be redirected to the 'Store contacts' tab, where you can edit your stores.</p>
 <p class="img-polaroid"><img src="img/tmgooglemap115-1.png" alt="" /></p>
-<p>To add a new store, press the 'Add new' button.</p>
+<p>To add a new store, press the 'Add new item' button.</p>
 <p class="img-polaroid"><img src="img/tmgooglemap115-2.png" alt="" /></p>
 <ul class="marked-list">
     <li><strong>Select a store</strong> - select the store that you want to add to the map.</li>
@@ -16,6 +16,7 @@
 <h5>Google Map display settings</h5>
 <p class="img-polaroid"><img src="img/tmgooglemap115-3.png" alt="" /></p>
 <ul class="marked-list">
+    <li><strong>Google Api Key</strong> - enter your Google Map API Key.</li>
     <li><strong>Map Style</strong> - select the map display style.</li>
     <li><strong>Map Type</strong> - select the map display type.</li>
     <li><strong>Zoom Level</strong> - specify the initial level of the map zoom (from 1 to 17).</li>
