1b1058ab51e32421556fecb7ff5af67e96bda04f
Nikki0 <victoria.panska@gmail.com>
2017-09-06 09:52:22 +0300

    ADD: Unminify .css file basic tutorial

---
+++ sections/basic-tutorials/en/unminify_css_file.php
@@ -0,0 +1,43 @@
+<h3>How to unminify .css file using the Sublime Text Editor</h3>
+<h4>Preparation</h4>
+<p>Before you proceed, please check the tutorial below on how to download and install the Sublime Text Editor and
+    Sublime Text Plugin that is responsible for the unminify process:</p>
+<ul class="marked-list">
+    <li>
+        <p>
+
+            <a target="_blank"
+               href="https://jetimpex.com/prestashop/support/how-to/prestashop-1-6-1-7-unminify-css-file-using-sublime-text-editor/">PrestaShop
+                1.6 and 1.7. How to unminify .css file using the Sublime Text Editor</a></p>
+    </li>
+</ul>
+<h4>Unminify .css file</h4>
+<p>Open the <strong>.css</strong> file in Sublime Text Editor (choose the <strong>.css</strong> file you need to
+    unminify, click the right mouse button on it and select <strong>Open with Sublime Text</strong> option (1)):
+</p>
+
+<figure class="img-polaroid">
+    <img src="img/css.png" alt=""/>
+</figure>
+
+<p>You can see the code is minified:</p>
+
+<figure class="img-polaroid">
+    <img src="img/sublime-text-1.png" alt=""/>
+</figure>
+
+<p>To unminify the code, select it all by clicking <strong>Ctrl+A</strong>. In top menu panel choose the <strong>Selection</strong>
+    tab and select there
+    <strong>Un-minify CSS</strong> option. Or simply click <strong>Ctrl+Alt+U</strong>:</p>
+
+<figure class="img-polaroid">
+    <img src="img/sublime-text-2.png" alt=""/>
+</figure>
+
+<p>Now, the code is unminified:</p>
+
+<figure class="img-polaroid">
+    <img src="img/sublime-text-3.png" alt=""/>
+
+
+
