a000323cab3840756f0394b3b5625653bae9985a
Nikki0 <victoria.panska@gmail.com>
2017-11-29 13:15:05 +0200

    EDIT: language switcher

--- index.php
+++ index.php
@@ -13,92 +13,107 @@ if (!isset($sections)) {
     <meta name="format-detection" content="telephone=no"/>
     <link rel="icon" href="<?php echo $projectFaviconPath; ?>" type="image/x-icon">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo get_path( '/css/grid.css' ); ?>">
+    <link rel="stylesheet" href="<?php echo get_path('/css/grid.css'); ?>">
 
-    <link rel="stylesheet" href="<?php echo get_path( '/css/style.css' ); ?>">
-    <link rel="stylesheet" href="<?php echo get_path( "/projects/{$project}/project_styles.css" ); ?>">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Montserrat+Alternates:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="<?php echo get_path('/css/style.css'); ?>">
+    <link rel="stylesheet" href="<?php echo get_path("/projects/{$project}/project_styles.css"); ?>">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Montserrat+Alternates:400,700'
+          rel='stylesheet' type='text/css'>
     <!--<link href='//fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>-->
-    <link rel="stylesheet" href="<?php echo get_path( '/css/prettify.css' ); ?>">
-    <link rel="stylesheet" href="<?php echo get_path( '/css/jquery.fancybox.css' ); ?>">
+    <link rel="stylesheet" href="<?php echo get_path('/css/prettify.css'); ?>">
+    <link rel="stylesheet" href="<?php echo get_path('/css/jquery.fancybox.css'); ?>">
 
-    <script src="<?php echo get_path( '/js/jquery.js' ); ?>"></script>
-    <script src="<?php echo get_path( '/js/jquery-migrate-1.2.1.js' ); ?>"></script>
+    <script src="<?php echo get_path('/js/jquery.js'); ?>"></script>
+    <script src="<?php echo get_path('/js/jquery-migrate-1.2.1.js'); ?>"></script>
 
     <!--[if lt IE 9]>
     <html class="lt-ie9">
     <div style='clear: both; text-align:center; position: relative;'>
         <a href="http://windows.microsoft.com/en-US/internet-explorer/">
-            <img src="<?php echo get_path( '/img/ie8-panel/warning_bar_0000_us.jpg' ); ?>" border="0" height="42" width="820"
+            <img src="<?php echo get_path( '/img/ie8-panel/warning_bar_0000_us.jpg' ); ?>" border="0" height="42"
+                 width="820"
                  alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
         </a>
     </div>
     <script src="/js/html5shiv.js"></script>
     <![endif]-->
 
-    <script src='<?php echo get_path( '/js/device.min.js' ); ?>'></script>
+    <script src='<?php echo get_path('/js/device.min.js'); ?>'></script>
 </head>
 <body data-section="<?php echo $section_param; ?>" onload="prettyPrint()" data-project="<?php echo $project; ?>">
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5WB7JQ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5WB7JQ');</script>
+<noscript>
+    <iframe src="//www.googletagmanager.com/ns.html?id=GTM-5WB7JQ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<script>(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(), event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-5WB7JQ');</script>
 <!-- End Google Tag Manager -->
 <div class="page-wrap">
     <div class="rd-mobilemenu active">
         <div class="logo logo__<?php echo $project; ?>">
-            <a href="<?php echo get_path( "/index.php?project={$project}" ); ?>">
+            <a href="<?php echo get_path("/index.php?project={$project}"); ?>">
                 <img src="<?php echo $projectImgLogoPath ?>" alt="<?php echo $projectTextLogo; ?>">
                 <?php echo $projectTextLogo; ?>
             </a>
         </div>
-          <div class="panel <?php if ($project != $defaultProject){?>hidden<?php } ?>">
-              <div class="select select-version">
-                  <select>
-                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-5/index_en.html">Version v1-5</option>
+        <div class="panel <?php if ($project != $defaultProject) { ?>shown<?php } ?>">
+            <div class="select select-version <?php if ($project != $defaultProject) { ?>hidden<?php } ?>">
+                <select>
+                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-5/index_en.html">Version v1-5
+                    </option>
                     <option data-href="#" selected>Version v1-3</option>
-                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-2/index_en.html">Version v1-2</option>
-                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-1/index_en.html">Version v1-1</option>
-                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-0/index_en.html">Version v1-0</option>
-                  </select>
-              </div>
-              <div class="select select-lang">
-                  <select>
-                      <option
-                              data-href="<?php echo $path; ?>index.php?lang=en&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "en" ? "selected" : ""; ?>>
-                          EN
-                      </option>
-                      <option
-                              data-href="<?php echo $path; ?>index.php?lang=de&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "de" ? "selected" : ""; ?>>
-                          DE
-                      </option>
-                      <option
-                              data-href="<?php echo $path; ?>index.php?lang=es&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "es" ? "selected" : ""; ?>>
-                          ES
-                      </option>
-                      <option
-                              data-href="<?php echo $path; ?>index.php?lang=fr&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "fr" ? "selected" : ""; ?>>
-                          FR
-                      </option>
-                      <option
-                              data-href="<?php echo $path; ?>index.php?lang=it&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "it" ? "selected" : ""; ?>>
-                          IT
-                      </option>
-                      <option
-                              data-href="<?php echo $path; ?>index.php?lang=pl&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "pl" ? "selected" : ""; ?>>
-                          PL
-                      </option>
-                      <option
-                              data-href="<?php echo $path; ?>index.php?lang=ru&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "ru" ? "selected" : ""; ?>>
-                          RU
-                      </option>
-                  </select>
-              </div>
-          </div>
+                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-2/index_en.html">Version v1-2
+                    </option>
+                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-1/index_en.html">Version v1-1
+                    </option>
+                    <option data-href="/help/quick-start-guide/prestashop-themes/v1-0/index_en.html">Version v1-0
+                    </option>
+                </select>
+            </div>
+            <div class="select select-lang">
+                <select>
+                    <option
+                            data-href="<?php echo $path; ?>index.php?lang=en&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "en" ? "selected" : ""; ?>>
+                        EN
+                    </option>
+                    <option
+                            data-href="<?php echo $path; ?>index.php?lang=de&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "de" ? "selected" : ""; ?>>
+                        DE
+                    </option>
+                    <option
+                            data-href="<?php echo $path; ?>index.php?lang=es&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "es" ? "selected" : ""; ?>>
+                        ES
+                    </option>
+                    <option
+                            data-href="<?php echo $path; ?>index.php?lang=fr&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "fr" ? "selected" : ""; ?>>
+                        FR
+                    </option>
+                    <option
+                            data-href="<?php echo $path; ?>index.php?lang=it&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "it" ? "selected" : ""; ?>>
+                        IT
+                    </option>
+                    <option
+                            data-href="<?php echo $path; ?>index.php?lang=pl&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "pl" ? "selected" : ""; ?>>
+                        PL
+                    </option>
+                    <option
+                            data-href="<?php echo $path; ?>index.php?lang=ru&section=<?php echo $section_param; ?>&project=<?php echo $project; ?>" <?php echo $lang == "ru" ? "selected" : ""; ?>>
+                        RU
+                    </option>
+                </select>
+            </div>
+        </div>
 
         <p class="copyright">TemplateMonster ©
             <a href="http://www.templatemonster.com/privacy-policy.php">Privacy Policy</a>
@@ -128,7 +143,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             =========================================================-->
             <main id="main">
                 <div class="container">
-                    <?php include_once get_path( '/section.php', true ); ?>
+                    <?php include_once get_path('/section.php', true); ?>
                 </div>
             </main>
 
@@ -153,18 +168,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 </div>
 
 
-<script src="<?php echo get_path( '/js/jquery.easing.1.3.js' ); ?>"></script>
-<script src="<?php echo get_path( '/js/jquery.rd-navbar.js' ); ?>"></script>
-<script src="<?php echo get_path( '/js/prettify.js' ); ?>"></script>
-<script src="<?php echo get_path( '/js/jquery.fancybox.js' ); ?>"></script>
-<script src="<?php echo get_path( '/js/jquery.ui.totop.js' ); ?>"></script>
-<script src="<?php echo get_path( '/js/responsive_tabs.min.js' ); ?>"></script>
+<script src="<?php echo get_path('/js/jquery.easing.1.3.js'); ?>"></script>
+<script src="<?php echo get_path('/js/jquery.rd-navbar.js'); ?>"></script>
+<script src="<?php echo get_path('/js/prettify.js'); ?>"></script>
+<script src="<?php echo get_path('/js/jquery.fancybox.js'); ?>"></script>
+<script src="<?php echo get_path('/js/jquery.ui.totop.js'); ?>"></script>
+<script src="<?php echo get_path('/js/responsive_tabs.min.js'); ?>"></script>
 
 <script>
     /* Section Class
      ========================================================*/
     function sectionClass(item_key) {
-        $.getJSON('<?php echo get_path( '/sections.json' ); ?>', function (json) {
+        $.getJSON('<?php echo get_path('/sections.json'); ?>', function (json) {
             var body = $('body');
             var section_class = 'scheme-' + item_key % 9;
             body.attr('class', '');
@@ -173,7 +188,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     }
 </script>
 
-<script src="<?php echo get_path( '/js/script.js' ); ?>"></script>
+<script src="<?php echo get_path('/js/script.js'); ?>"></script>
 
 </body>
 </html>
