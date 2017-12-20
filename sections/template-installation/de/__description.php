3f470af45ef17a8a71e168bff6223d174606d8aa
TM\nikki <victoria.panska@gmail.com>
2017-12-20 14:39:34 +0200

    ADD: template installation notification

--- sections/template-installation/de/__description.php
+++ sections/template-installation/de/__description.php
@@ -1,1 +1,4 @@
 <h2 class="item3">Installation der Vorlage</h2>
+<p class="alert alert-info custom">Wenn Sie einiges an Zeit sparen möchten, <span class="label label-warning opener"
+                                                                                  onclick="showChat()">bestellen</span>
+    Sie den Installationsservice für Ihr Theme in unserem Online-Chat.</p>
--- sections/template-installation/it/__description.php
+++ sections/template-installation/it/__description.php
@@ -1,1 +1,4 @@
 <h2 class="item3">Installazione Template</h2>
+<p class="alert alert-info custom">Se non vuoi spendere il tempo per l'installazione del modello, non esitare a
+    contattarci <span class="label label-warning opener" onclick="showChat()">ordina</span> servizio di installaione
+    utilizzando il nostro live-chat.</p>
