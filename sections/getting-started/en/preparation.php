ab6e165e77a037fb5985170e722f20bd9e25a5e3
alex ross <alex.ross.th@gmail.com>
2018-01-25 15:57:43 +0200

    SG offer
    Signed-off-by: alex ross <alex.ross.th@gmail.com>

--- sections/getting-started/en/preparation.php
+++ sections/getting-started/en/preparation.php
@@ -1,2 +1,4 @@
 <h3>Preparation</h3>
 <p>Before you proceed to setting up your PrestaShop e-commerce, please make sure you are fully prepared. Please check the following points:</p>
+
+<p class="alert alert-info custom">If you don't want to spend your time installing the template, don't hesitate to <span class="label label-warning opener" onclick="showChat()">order</span> installation service using our live-chat.</p>
--- sections/getting-started/en/server-requirements.php
+++ sections/getting-started/en/server-requirements.php
@@ -3,10 +3,18 @@
 <ul class="marked-list">
     <li><strong>System:</strong> Unix, Linux or Windows. Unix is highly recommended.</li>
     <li><strong>Web server:</strong> Apache Web server 1.3 or later, Microsoft's IIS Web server 6.0 or later, and nginx 1.0 or later.
-    </li>
+	</li>
     <li><strong>PHP 5.4</strong> or later.</li>
     <li><strong>MySQL 5.5</strong> or later.</li>
     <li>At least 128 Mb of RAM on your server (64 Mb is comfortable, the more the better).
-    </li>
+	</li>
 </ul>
 <p class="alert alert-info">PrestaShop will work with PHP 5.1 or later, but the versions lower than 5.4 have bugs which might prevent some functions from working as expected (like invalid date timezone).</p>
+
+<h4>Need a solid hosting provider? We recommend SiteGround!</h4>
+
+<p style="text-align:center;">
+	<a class="leadlink" target="_blank" href="https://goo.gl/VyS3yQ"><img src="https://www.templatemonster.com/help/wp-content/uploads/2018/01/SG_banner.png" alt="SiteGround Hosting">
+	</a>
+</p>
+<h5 style="text-align:center;"><strong> High performance platform suitable for projects of all sizes</strong></h5>
