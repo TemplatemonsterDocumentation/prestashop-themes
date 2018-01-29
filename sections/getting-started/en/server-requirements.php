6e0ac975c866f53f3824c88e4155718c69d4a525
Nikki0 <victoria.panska@gmail.com>
2018-01-29 12:43:39 +0200

    ADD: SG offer

--- sections/getting-started/en/server-requirements.php
+++ sections/getting-started/en/server-requirements.php
@@ -2,11 +2,19 @@
 <p>Please make sure your hosting server meets the PrestaShop requirements:</p>
 <ul class="marked-list">
     <li><strong>System:</strong> Unix, Linux or Windows. Unix is highly recommended.</li>
-    <li><strong>Web server:</strong> Apache Web server 1.3 or later, Microsoft's IIS Web server 6.0 or later, and nginx 1.0 or later.
+    <li><strong>Web server:</strong> Apache Web server 1.3 or later, Microsoft's IIS Web server 6.0 or later, and nginx
+        1.0 or later.
     </li>
     <li><strong>PHP 5.4</strong> or later.</li>
     <li><strong>MySQL 5.5</strong> or later.</li>
     <li>At least 128 Mb of RAM on your server (64 Mb is comfortable, the more the better).
     </li>
 </ul>
-<p class="alert alert-info">PrestaShop will work with PHP 5.1 or later, but the versions lower than 5.4 have bugs which might prevent some functions from working as expected (like invalid date timezone).</p>
+<p class="alert alert-info">PrestaShop will work with PHP 5.1 or later, but the versions lower than 5.4 have bugs which
+    might prevent some functions from working as expected (like invalid date timezone).</p>
+<h4>Need a solid hosting provider? We recommend SiteGround!</h4>
+<p style="text-align:center;">
+    <a target="_blank" href="https://goo.gl/VyS3yQ"><img src="https://www.templatemonster.com/help/wp-content/uploads/2018/01/SG_banner.png" alt="SiteGround Hosting">
+    </a>
+</p>
+<h5 style="text-align:center;"><strong>High performance platform suitable for projects of all sizes</strong></h5>
--- sections/getting-started/pl/server-requirements.php
+++ sections/getting-started/pl/server-requirements.php
@@ -12,3 +12,11 @@
 <p class="alert alert-info">PrestaShop będzie działał z PHP 5.1 lub nowszą wersją, jednakże wersje poniżej 5.4 mają
     błędy które mogą uniemożliwić korzystanie z niektórych funkcji w oczekiwany sposób i z oczekiwaną wydajnością (jak
     na przykład nieważna data lub strefa czasowa).</p>
+<h4>Szukasz wiarygodnego dostawcę usług hostingowych? Polecamy hosting SiteGround!</h4>
+<p style="text-align:center;">
+    <a target="_blank" href="https://goo.gl/VyS3yQ"><img
+                src="https://www.templatemonster.com/help/wp-content/uploads/2018/01/SG_banner.png"
+                alt="SiteGround Hosting">
+    </a>
+</p>
+<h5 style="text-align:center;"><strong>Wydajna platforma do uruchomienia serwisów o dowolnym rozmiarze</strong></h5>
