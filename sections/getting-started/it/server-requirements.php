99f8358e20eb0b5dfbded59a44d71866cd4d5d0b
TM\nikki <victoria.panska@gmail.com>
2018-02-05 10:15:28 +0200

    REMOVE: SG offer

--- sections/getting-started/it/server-requirements.php
+++ sections/getting-started/it/server-requirements.php
@@ -16,12 +16,12 @@
 <p class="alert alert-info"> PrestaShop funziona con PHP 5.1 o successivi, ma le versioni inferiori alla 5.2 hanno
     dei bug che potrebbero impedire il corretto funzionamento di alcune funzioni (ad esempio problemi di orari e
     fusi orari).</p>
-<h4>Hai bisogno di un solido fornitore di hosting? Raccomandiamo SiteGround!</h4>
-<p style="text-align:center;">
-    <a target="_blank" href="https://goo.gl/VyS3yQ"><img
-                src="https://www.templatemonster.com/help/wp-content/uploads/2018/01/SG_banner.png"
-                alt="SiteGround Hosting">
-    </a>
-</p>
-<h5 style="text-align:center;"><strong>Piattaforma ad alte prestazioni adatta a progetti di tutte le dimensioni</strong>
-</h5>
+<!--<h4>Hai bisogno di un solido fornitore di hosting? Raccomandiamo SiteGround!</h4>-->
+<!--<p style="text-align:center;">-->
+<!--    <a target="_blank" href="https://goo.gl/VyS3yQ"><img-->
+<!--                src="https://www.templatemonster.com/help/wp-content/uploads/2018/01/SG_banner.png"-->
+<!--                alt="SiteGround Hosting">-->
+<!--    </a>-->
+<!--</p>-->
+<!--<h5 style="text-align:center;"><strong>Piattaforma ad alte prestazioni adatta a progetti di tutte le dimensioni</strong>-->
+<!--</h5>-->
